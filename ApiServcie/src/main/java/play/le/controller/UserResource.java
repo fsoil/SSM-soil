131612973866e6bcf2b130c97e32f8e278628cfd
fuzhenhui <fhammer@qq.com>
2018-01-26 15:42:44 +0800

    基本整合

---
+++ ApiServcie/src/main/java/play/le/controller/UserResource.java
@@ -0,0 +1,21 @@
+package play.le.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import play.le.model.User;
+
+/**
+ * Created by fuzh2 on 2018/1/26.
+ */
+
+@Controller
+@RequestMapping("/users")
+public class UserResource {
+
+    @RequestMapping("/user")
+    @ResponseBody
+    public User getUser() {
+        return new User("hammer", 18);
+    }
+}
